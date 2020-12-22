0da9163a68c52ea4288b4759bd9cc92ae2bc917f
ex1mp <46762072+ex1mp@users.noreply.github.com>
2020-12-23 01:32:36 +0300

    Добавьте файлы проекта.

---
+++ TextParser/Texts/TextItems/Sentences/SentenceItems/ImplementingClasses/Punctuations/IPunctuation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextParser.Texts.TextItems.Sentences.SentenceItems.ImplementingClasses.Punctuations
+{
+    public interface IPunctuation
+    {
+    }
+}
---
+++ TextParser/Texts/TextItems/Sentences/SentenceItems/ImplementingClasses/Punctuations/ImplementingClasses/Punctuation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextParser.Texts.TextItems.Sentences.SentenceItems.ImplementingClasses.Punctuations.ImplementingClasses
+{
+    public class Punctuation: IPunctuation
+    {
+    }
+}
